0a8e5fd7b805a6a84f6bd58e603dee8470c76273
Sachin DN <sachugowda@hotmail.com>
2024-04-23 12:38:57 +0530

    Create switch.c

---
+++ Foundation/switch.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int day;
+
+    // Prompt the user to enter a day number
+    printf("Enter a day number (1-7): ");
+    scanf("%d", &day);
+
+    switch (day) {
+        case 1:
+            printf("Monday");
+            break;
+        case 2:
+            printf("Tuesday");
+            break;
+        case 3:
+            printf("Wednesday");
+            break;
+        case 4:
+            printf("Thursday");
+            break;
+        case 5:
+            printf("Friday");
+            break;
+        case 6:
+            printf("Saturday");
+            break;
+        case 7:
+            printf("Sunday");
+            break;
+        default:
+            printf("Invalid day number");
+    }
+
+    return 0;
+}
